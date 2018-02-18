4c7b6ac6e77f391d0a7ba7af41e035bf0a752701
Theo Qui <qt500641@etu.unice.fr>
2018-02-18 15:55:53 +0100

    restructuration du projet

---
+++ android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/MenuPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.utilisateur.projetl3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Utilisateur on 14/02/2018.
+ */
+public class MenuPrincipal extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
