fe32efe385bf8fb8c298b1927da4c5e6f656b82f
Theo Qui <qt500641@etu.unice.fr>
2018-02-21 11:08:58 +0100

    début serveur (LPEPD-9)

--- javastd/serveur/src/main/java/Server.java
+++ javastd/serveur/src/main/java/Server.java
@@ -2,17 +2,32 @@ import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
 
 public class Server{
     private Configuration config;
-    private ConnectListener listener;
+    private ConnectListener connectListener;
+    private DisconnectListener disconnectListener;
+
     public Server(String hostname, int port) {
         config = new Configuration();
         config.setHostname(hostname);
         config.setPort(port);
-        listener = new ConnectListener() {
+        connectListener = new ConnectListener() {
             public void onConnect(SocketIOClient client) {
-                
+                StringBuilder builder = new StringBuilder();
+                builder.append(client);
+                builder.append(" s'est connecté");
+                System.out.println(builder.toString());
+            }
+        };
+
+        disconnectListener = new DisconnectListener() {
+            public void onDisconnect(SocketIOClient client) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("Déconnexion du client ");
+                builder.append(client);
+                System.out.println(builder.toString());
             }
         };
     }
@@ -20,6 +35,7 @@ public class Server{
     public void run() {
         final SocketIOServer server = new SocketIOServer(config);
         server.start();
-        server.addConnectListener();
+        server.addConnectListener(connectListener);
+        server.addDisconnectListener(disconnectListener);
     }
 }
