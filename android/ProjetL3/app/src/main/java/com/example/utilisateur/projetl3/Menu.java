f1036cd98de0201bac465af9fc221f8be1809c44
Theo Qui <qt500641@etu.unice.fr>
2018-02-21 12:59:48 +0100

    Récupération de l'identifiant depuis les champs de texte (le mot de passe n'est pas traité (LPEPD-6)

--- android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/Menu.java
+++ android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/Menu.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Menu extends AppCompatActivity {
 
@@ -19,6 +20,8 @@ public class Menu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Menu.this, MenuPrincipal.class);
+                EditText pseudo = findViewById(R.id.textPseudo);
+                intent.putExtra("login", pseudo.getText().toString());
                 startActivity(intent);
             }
         });
--- android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/MenuPrincipal.java
+++ android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/MenuPrincipal.java
@@ -2,6 +2,7 @@ package com.example.utilisateur.projetl3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Utilisateur on 14/02/2018.
@@ -11,5 +12,9 @@ public class MenuPrincipal extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        String login = getIntent().getStringExtra("login");
+
+        TextView view = findViewById(R.id.textPseudo);
+        view.setText(login);
     }
 }
