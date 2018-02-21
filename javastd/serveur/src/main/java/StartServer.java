050e2d0db9c36c6d60006fac75df279747cada46
Theo Qui <qt500641@etu.unice.fr>
2018-02-21 12:21:39 +0100

    Serveur basique (LPEPD-9)

--- javastd/serveur/src/main/java/StartServer.java
+++ javastd/serveur/src/main/java/StartServer.java
@@ -1,12 +1,15 @@
 public class StartServer {
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 2) {
             System.err.println("Syntaxe : StartServer hostname port");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(i + " :" + args[i]);
+            }
             System.exit(-1);
         } else {
             try {
-                int port = Integer.parseInt(args[2]);
-                Server serveur = new Server(args[1], port);
+                int port = Integer.parseInt(args[1]);
+                Server serveur = new Server(args[0], port);
                 serveur.run();
             }
             catch (NumberFormatException e) {
