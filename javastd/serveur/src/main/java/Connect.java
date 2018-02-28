4ab71243c81ab4b82479af441bae744cfc495202
fb502902 <brandon0683@hotmail.fr>
2018-02-28 13:07:33 +0100

    connection en cours (LPEPD-18)

---
+++ javastd/serveur/src/main/java/Connect.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver O.K.");
+
+            String url = "jdbc:mysql://mysql.hostinger.fr/u606391292_lpepd";
+            String user = "u606391292_root0";
+            String passwd = "tauub>=WkhAUG3S~h";
+
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+
+            System.out.println("Connexion effective !");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
