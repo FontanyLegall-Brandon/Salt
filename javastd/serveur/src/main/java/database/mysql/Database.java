a28f31df330f37674e6f9eb33ec1a6abba983f7b
fb502902 <brandon0683@hotmail.fr>
2018-03-14 11:00:33 +0100

    LPEPD-25 Interactions Serveur-Database

--- javastd/serveur/src/main/java/database/mysql/Database.java
+++ javastd/serveur/src/main/java/database/mysql/Database.java
@@ -5,7 +5,8 @@ public class Database implements database.Database {
   //TODO Implementer cette classe qui permet de faire l'interface entre la  database et le serveur
   
     public void addUser(String nom) {
-        System.out.println("test");
+
     }
+
   
 }
