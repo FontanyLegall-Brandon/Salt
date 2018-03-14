efc81a2203b51ad53c43b107937846f5eeb5a051
fb502902 <brandon0683@hotmail.fr>
2018-03-14 13:00:58 +0100

    LPEPD-27 Connexion à la database en cours ...

--- javastd/serveur/src/main/java/database/mysql/Connect.java
+++ javastd/serveur/src/main/java/database/mysql/Connect.java
@@ -3,10 +3,44 @@ package database.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import java.sql.* ;
+
 public class Connect {
 
+    // JDBC driver name and database URL
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://mysql.hostinger.fr/u606391292_lpepd";
+
+    //  Database credentials
+    private static final String USER = "u606391292_root0";
+    private static final String PASS = "tauub>=WkhAUG3S~h";
+
+
     public static Connection connection(){
+
         Connection conn = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+
+        return conn;
+
+
+        /*
+        Connection conn = null;
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver O.K.");
@@ -25,6 +59,7 @@ public class Connect {
         }
 
         return conn;
+        */
     }
 
     public static void main(String[] args) {
