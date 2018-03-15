64a4eb1e552ab8dccfc93d894d003ae6222c666d
Loïc Germerie <lg@MacBook-de-Loic.local>
2018-03-15 01:13:52 +0100

    Première ébauche des tests unitaires de la classe Server LPEPD-24

---
+++ javastd/serveur/src/test/java/listener/TestNewUser.java
@@ -0,0 +1,24 @@
+package listener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import serveur.Server;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestNewUser {
+
+    @Mock Server server;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testOnData() {
+
+    }
+}
---
+++ javastd/serveur/src/test/java/serveur/TestServer.java
@@ -0,0 +1,63 @@
+package serveur;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import listeners.NewUserListener;
+import listeners.mappers.UserInfo;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.net.URISyntaxException;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestServer {
+
+    @Mock private NewUserListener newUserListener;  // Création d'un faux NewUserListener
+
+    @InjectMocks Server server = new Server("localhost", 10101);   // qu'on injecte dans un faux serveur
+
+    @Mock UserInfo info;
+
+    Socket mSocket;
+
+    @Before
+    public void setUp() {
+        try {
+            mSocket = IO.socket("localhost:10101");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        info = new UserInfo();
+    }
+
+    /*@Test
+    public void testConnexion() {
+        Server server = new Server("localhost", 1345);
+        server.run();
+        assertTrue(server.isRunning());//vérifie que le serveur tourne lorsqu'il est lancé
+        server.stop();
+        assertFalse(server.isRunning());//vérifie que le serveur s'est bien arrêté
+    }*/
+
+    @Test
+    public void testNewUser() {
+        mSocket.connect();
+        mSocket.emit("newUser", info);
+
+        //verify(newUserListener, timeout(1000)).onData(any(), eq(info), any());
+
+    }
+}
