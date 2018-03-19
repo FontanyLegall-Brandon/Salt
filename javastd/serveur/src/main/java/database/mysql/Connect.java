19c5732786d5831896c43894315f6e082f3f5b3f
fb502902 <brandon0683@hotmail.fr>
2018-03-19 10:04:52 +0100

    connection database (LPEPD-27)

--- javastd/serveur/src/main/java/database/mysql/Connect.java
+++ javastd/serveur/src/main/java/database/mysql/Connect.java
@@ -5,17 +5,12 @@ import java.sql.DriverManager;
 
 import java.sql.* ;
 
+/**
+ * La classe Connect permet de se connecter à la base de donnée distant grace à la librairie JDBC.
+ * Driver JDBC : com.mysql.jdbc.Driver
+ */
 public class Connect {
 
-    /*
-    // JDBC driver name and database URL
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://mysql.hostinger.fr/u606391292_lpepd";
-
-    //  Database credentials
-    private static final String USER = "u606391292_root0";
-    private static final String PASS = "tauub>=WkhAUG3S~h";
-    */
 
     // JDBC driver name and database URL
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
@@ -25,7 +20,10 @@ public class Connect {
     private static final String USER = "lpepd";
     private static final String PASS = "qt8i244AZUsA";
 
-
+    /**
+     * La fonction permet la connexion à la database
+     * @return objet du type Connection qui va être passé à la classe Database
+     */
     public static Connection connection(){
 
         Connection conn = null;
@@ -46,30 +44,6 @@ public class Connect {
         }
 
         return conn;
-
-
-        /*
-        Connection conn = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("Driver O.K.");
-
-            String url = "jdbc:mysql://mysql.hostinger.fr/u606391292_lpepd";
-            String user = "u606391292_root0";
-            String passwd = "tauub>=WkhAUG3S~h";
-
-            conn = DriverManager.getConnection(url, user, passwd);
-
-            System.out.println("Connexion effective !");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        return conn;
-        */
     }
 
     public static void main(String[] args) {
