6eb36b35333e8f8dafa11086f287d8f65b44be15
Theo Qui <qt500641@etu.unice.fr>
2018-03-21 09:50:21 +0100

    Listener de réponse du serveur (LPEPD-7)

---
+++ javastd/serveur/src/main/java/listeners/mappers/Reply.java
@@ -0,0 +1,13 @@
+package listeners.mappers;
+
+public class Reply {
+    private final String message;
+
+    public Reply(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
