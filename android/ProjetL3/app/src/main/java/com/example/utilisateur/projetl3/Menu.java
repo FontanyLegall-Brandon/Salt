5f9034c88d2f10f59ad399b8202a45e6a75c96ee
Loïc Germerie <lg@MacBook-de-Loic.local>
2018-04-17 17:49:10 +0200

    Gestion du login sur l'écran principal LPEPD-2

--- android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/Menu.java
+++ android/ProjetL3/app/src/main/java/com/example/utilisateur/projetl3/Menu.java
@@ -18,19 +18,41 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class Menu extends ActivityForIO {
 
+    private boolean waitingLoginReply;
+
+    public boolean isWaitingLoginReply() {
+        return waitingLoginReply;
+    }
+
+    public void setWaitingLoginReply(boolean waitingLoginReply) {
+        this.waitingLoginReply = waitingLoginReply;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        // Selection du layout
         setContentView(R.layout.activity_menu);
+
+        // Réccupération de toutes les vues
         final EditText pseudo = (EditText) findViewById(R.id.etPseudo);
         final EditText password = (EditText) findViewById(R.id.etMDP);
         final Button loginButton = (Button) findViewById(R.id.buttonValid);
         final TextView register = (TextView) findViewById(R.id.tvSinscrireici);
-        final Button playbutton = (Button) findViewById(R.id.playbutton);
-        findViewById(R.id.buttonValid).setEnabled(false);
+        final Button playButton = (Button) findViewById(R.id.playbutton);
+
+        // Par défaut le bouton Jouer est blanc
+        playButton.setBackgroundColor(Color.WHITE);
+
+        // Par défaut le bouton de connexion est inactif, il faut taper du texte pour l'activer
+        loginButton.setEnabled(false);
         loginButton.setBackgroundColor(Color.GRAY);
 
-        TextWatcher textWatcher = new TextWatcher() {
+        // Permet d'activer le bouton de connexion quand on tape du texte
+        TextWatcher activateButtonTextWatcher = new TextWatcher() {
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -53,9 +75,11 @@ public class Menu extends ActivityForIO {
             }
         };
 
-        pseudo.addTextChangedListener(textWatcher);
-        password.addTextChangedListener(textWatcher);
+        // Application du textWatcher aux deux champs
+        pseudo.addTextChangedListener(activateButtonTextWatcher);
+        password.addTextChangedListener(activateButtonTextWatcher);
 
+        // Listener permettant de passer à la vue d'inscription en cliquant sur le bouton Inscription
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,49 +87,64 @@ public class Menu extends ActivityForIO {
                 Menu.this.startActivity(registerIntent);
             }
         });
+
+        // Listener du bouton de login
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {//quand on clique sur le bouton login
-                if (Singleton.CLIENT.is_connected()) {
-                    Intent intent = new Intent(Menu.this, MenuPrincipal.class);
-                    EditText pseudo = findViewById(R.id.etPseudo);//pseudo récupéré du champs de texte
-                    intent.putExtra("login", pseudo.getText().toString());
+
+                if (isWaitingLoginReply()) { // Si le bouton est en état d'annulation
+                    // On remet les boutons dans leur configuration et on annule l'attente
+                    stopWaitingForLoginReply(loginButton, playButton);
+                    return; // Le reste de la méthode concerne le cas où on attend pas la réponse
+                }
+
+                // Désactivation du bouton de passage au menu de jeu
+                playButton.setEnabled(false);
+                playButton.setBackgroundColor(Color.GRAY);
+                // Le bouton de connexion devient un bouton d'annulation
+                loginButton.setText("Cancel");
+
+                // On attend la réponse
+                setWaitingLoginReply(true);
+
+                if (Singleton.CLIENT.is_connected()) { // Si la connexion avec le serveur est active
+                    // On lance l'opération de login sur le serveur
                     Singleton.CLIENT.sendLogin(pseudo.getText().toString(), password.getText().toString());
-                } else {
+                    // Un listener du client se chargera de gérer le retour du serveur
+                }
+                else { // On est pas connecté
+                    // On n'attends pas de réponse, donc on remet les boutons dans leur configuration de base
+                    stopWaitingForLoginReply(loginButton, playButton);
+                    // Et on signifie le problème de connexion à l'utilisateur
                     Toast.makeText(getApplicationContext(), "Vous n'êtes pas connecté au serveur", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        playbutton.setOnClickListener(new View.OnClickListener() {
+        // Listener du bouton Jouer, qui nous envoit simplement dans la vue menue principale
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Menu.this, MenuPrincipal.class);
                 startActivity(intent);
             }
         });
-        super.onCreate(savedInstanceState);
+
+        // Configuration des polices
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/sfcomicscriptnormal.ttf")
                 .setFontAttrId(R.attr.fontPath)
                 .build()
         );
-    Singleton client = Singleton.CLIENT;
-    client.setActivity(this);//définir l'activité utilisée par le singleton sur l'activité courante
-    }
-    private final class CancelOnClickListener implements
-            DialogInterface.OnClickListener {
-        public void onClick(DialogInterface dialog, int which) {
-            Toast.makeText(getApplicationContext(), "Connexion en cours...",
-                    Toast.LENGTH_LONG).show();
-        }
+
+        Singleton.CLIENT.setActivity(this); //définir l'activité utilisée par le singleton sur l'activité courante
     }
 
-
-    /*private final class OkOnClickListener implements
-            DialogInterface.OnClickListener {
-        public void onClick(DialogInterface dialog, int which) {
-            pass=true;
-        }
-    }*/
+    void stopWaitingForLoginReply(Button loginButton, Button playbutton) {
+        loginButton.setText("Se connecter");
+        playbutton.setBackgroundColor(Color.WHITE);
+        playbutton.setEnabled(true);
+        setWaitingLoginReply(false);
+    }
 }
